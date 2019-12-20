1f56526b65af086ce0090c43f2623a05511ad238
Ziv <ziv.israeli01@gmail.com>
2019-12-20 14:04:56 -0800

    Updates

---
+++ precache-manifest.f10ceddad96ad06ba3d6b9be26eefd34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a566cc57b86040913268aef42c51304",
+    "url": "/miareact/index.html"
+  },
+  {
+    "revision": "9c6dd23ee54b50110a4a",
+    "url": "/miareact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "40309107c14576f5ad01",
+    "url": "/miareact/static/js/2.779d23dd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/miareact/static/js/2.779d23dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c6dd23ee54b50110a4a",
+    "url": "/miareact/static/js/main.11145734.chunk.js"
+  },
+  {
+    "revision": "144e692c7be4a3143b39",
+    "url": "/miareact/static/js/runtime-main.f4da9771.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/miareact/static/media/logo.5d5d9eef.svg"
+  }
+]);
